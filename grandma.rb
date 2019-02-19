2de4944514bf53d3edf061e5f0b4815782f45a79
Marie Berry (Leugermann) <marie@creativeos.io>
2019-02-19 01:41:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
-input_phrase = "Hi, how are you?"
-
 def speak_to_grandma(phrase)  
+
+input_phrase = "I LOVE YOU GRANDMA!"
+
   if (phrase == phrase.upcase) == false
     puts "HUH?! SPEAK UP, SONNY!" 
   end
@@ -9,6 +10,20 @@ def speak_to_grandma(phrase)
   end
 end
 
+dog = "cuddly"
+ 
+if dog == "hungry"
+  puts "Refilling food bowl."
+elsif dog == "thirsty"
+  puts "Refilling water bowl."
+elsif dog == "playful"
+  puts "Playing tug-of-war."
+elsif dog == "cuddly"
+  puts "Snuggling."
+else
+  puts "Reading newspaper."
+end
+ 
   
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
