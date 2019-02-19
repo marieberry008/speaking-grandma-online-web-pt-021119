13f02e6f43b95aa6fbf7276772bd25fce848a9b6
Marie Berry (Leugermann) <marie@creativeos.io>
2019-02-19 01:44:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,6 +6,8 @@ phrase = "I LOVE YOU GRANDMA!"
     puts "HUH?! SPEAK UP, SONNY!" 
   elsif (phrase == phrase.upcase) == true
     puts "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   end
   
 end
